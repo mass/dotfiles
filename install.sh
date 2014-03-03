ca5c78019297ae281a8b9bec4a9da4e4aee1dd6f
Andrew Mass <amass1212@gmail.com>
2014-03-02 21:35:44 -0600

    Update installation script.

--- install.sh
+++ install.sh
@@ -1,50 +1,30 @@
 #!/bin/bash
 
-# Installs the configuration for zsh, vim, and more that @mass uses.
-
-usage() {
-  echo "Usage: ./install.sh [-b] [-z]" >&2
-  echo "       -b  Install bash configuration" >&2
-  echo "       -z  Install zsh configuration" >&2
-  exit 1
-}
-
-# Parses input options
-SHELL=""
-while getopts "bz" opt; do
-  case $opt in
-    b)
-      SHELL="BASH"
-      ;;
-    z)
-      SHELL="ZSH"
-      ;;
-  esac
-done
-
-# No environment was set in the arguments
-if [[ -z $SHELL ]]; then
-  usage
-fi
+# Installs @mass's configuration for zsh, bash, vim, and more. 
 
 DOTDIR=$(pwd)
 VIMDIR=$(pwd)/vim
 BASHDIR=$(pwd)/bash
 ZSHDIR=$(pwd)/zsh
 
+# Installs zshrc
+ln -i -s $ZSHDIR/zshrc ~/.zshrc
+
+# Installs bashrc
+ln -i -s $BASHDIR/bashrc ~/.bashrc
+
 # Installs vim
-cd ~
-ln -i -s $DOTDIR/vimrc .vimrc
-ln -i -s $VIMDIR .vim
+ln -i -s $DOTDIR/vimrc ~/.vimrc
+ln -i -s $VIMDIR ~/.vim
 cd $VIMDIR
+rm vim
 git submodule update --init
 
 # Installs redshift.conf
 if [ ! -d ~/.config ]; then
   mkdir ~/.config
 fi
-cd ~
-ln -i -s $DOTDIR/redshift.conf .config/redshift.conf
+ln -i -s $DOTDIR/redshift.conf ~/.config/redshift.conf
 
 # Installs .gitconfig
-ln -i -s $DOTDIR/gitconfig .gitconfig
+ln -i -s $DOTDIR/gitconfig ~/.gitconfig
