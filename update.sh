5ed88ad7314c48fe9a0612894c6b495ca7a3329d
Andrew Mass <amass1212@gmail.com>
2014-03-12 21:03:31 -0500

    Add update script. Update vim.

---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Updates all files to the latest versions.
+
+DOTDIR=$(pwd)
+VIMDIR=$(pwd)/vim
+BASHDIR=$(pwd)/bash
+ZSHDIR=$(pwd)/zsh
+
+cd $DOTDIR
+git pull origin master
+git submodule update
+git submodule foreach git pull origin master
+git submodule foreach git submodule update
+
+cd $VIMDIR
+git pull origin master
+git submodule update
+git submodule foreach git pull origin master
+
+cd $ZSHDIR
+git pull origin master
+
+cd $DOTDIR
